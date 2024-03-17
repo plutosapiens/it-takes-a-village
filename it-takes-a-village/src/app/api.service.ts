43fe6ee3c24b5b337ff084670f6580ddf25dcec6
Pluto <96972840+plutosapiens@users.noreply.github.com>
2024-03-17 12:56:24 +0200

    IVT - Generate environment

--- it-takes-a-village/src/app/api.service.ts
+++ it-takes-a-village/src/app/api.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,11 @@ import { Injectable } from '@angular/core';
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getCatalog() {
+    const { apiUrl } = environment;
+
+    return this.http.get<any>(`${apiUrl}/catalog`);
+  }
 }
