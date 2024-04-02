b36f8374f232949f0620f671366df88b71d0d4f5
Pluto <96972840+plutosapiens@users.noreply.github.com>
2024-04-02 09:29:55 +0300

    ITV - Display home page

--- it-takes-a-village/src/app/core/core.module.ts
+++ it-takes-a-village/src/app/core/core.module.ts
@@ -3,18 +3,20 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { SharedModule } from '../shared/shared.module';
+import { HomeComponent } from './home/home.component';
 
 
 
 @NgModule({
   declarations: [
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
   ],
   imports: [
     CommonModule,
     SharedModule
   ],
-  exports: [HeaderComponent, FooterComponent],
+  exports: [HeaderComponent, FooterComponent, HomeComponent],
 })
 export class CoreModule { }
