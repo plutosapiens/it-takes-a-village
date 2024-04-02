eac29e2870fd28582b07caedc9f4e71c45c8ad65
Pluto <96972840+plutosapiens@users.noreply.github.com>
2024-04-02 15:42:41 +0300

    ITV - remove main component, fix footer position

--- it-takes-a-village/src/app/app.module.ts
+++ it-takes-a-village/src/app/app.module.ts
@@ -1,28 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
-import { MainComponent } from './main/main.component';
-import { CatalogComponent } from './catalog/catalog.component';
-import { HttpClientModule } from '@angular/common/http';
+
 import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireAuthModule }  from '@angular/fire/compat/auth'; // Import for Firebase Authentication (optional)
-// Import other Firebase modules as needed (Firestore, Storage, etc.)
+import { AngularFireAuthModule }  from '@angular/fire/compat/auth';
+import { AppRoutingModule } from './app-routing.module';
 import { environment } from '../environments/environment.development';
 
+import { CatalogComponent } from './catalog/catalog.component';
 import { LoginComponent } from './login/login.component';
 import { FormsModule } from '@angular/forms';
 import { SignupComponent } from './signup/signup.component';
-import { Router } from '@angular/router';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent,
     CatalogComponent,
     LoginComponent,
     SignupComponent,
@@ -31,8 +25,7 @@ import { Router } from '@angular/router';
     BrowserModule,
     AppRoutingModule,
     CoreModule,
-    SharedModule,
-    HttpClientModule,    
+    SharedModule,   
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
    FormsModule,
