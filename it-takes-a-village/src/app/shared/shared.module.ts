273cf90bd390fee84b719dd66632031a83c199b4
Pluto <96972840+plutosapiens@users.noreply.github.com>
2024-04-04 11:50:51 +0300

    ITV - Add cute loader on place of the button while uploading image

--- it-takes-a-village/src/app/shared/shared.module.ts
+++ it-takes-a-village/src/app/shared/shared.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LogoutComponent } from './logout/logout.component';
+import { LoaderComponent } from './loader/loader.component';
 
 
 
 @NgModule({
   declarations: [
     LogoutComponent,
+    LoaderComponent,
   ],
   imports: [
     CommonModule,
   ],
   exports: [
     LogoutComponent,
+    LoaderComponent
   ]
 })
 export class SharedModule { }
