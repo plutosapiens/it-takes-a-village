606cfc33b2cd2c4f6bf9c7399e35e350c20cc99f
Pluto <96972840+plutosapiens@users.noreply.github.com>
2024-04-04 20:09:55 +0300

    ITV - Display appropriate buttons

--- it-takes-a-village/src/app/tips/article/article.component.ts
+++ it-takes-a-village/src/app/tips/article/article.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { ActivatedRoute } from '@angular/router';
+import { take } from 'rxjs';
+import { ApiService } from 'src/app/api.service';
+import { AuthService } from 'src/app/services/auth.service';
 import { Post } from 'src/app/types/post';
 
 @Component({
@@ -10,15 +13,46 @@ import { Post } from 'src/app/types/post';
 })
 export class ArticleComponent implements OnInit {
   post: Post | null = null;
+  postId: string | null = null;
+
+  isOwner: boolean = false;
 
   constructor(
     private route: ActivatedRoute, 
-    private firestore: AngularFirestore
+    private firestore: AngularFirestore,
+    private authService: AuthService,
+    private apiService: ApiService,
     ) {}
 
   urlId: string = ''
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    let currentUserId: string = '';
+    const user = await this.authService.getCurrentUser().pipe(take(1)).toPromise();
+    currentUserId = user ? user.uid : 'idk';
+    console.log('1currentuser id:', currentUserId )
+    this.handleUserData(currentUserId);
+    console.log("isowner", this.isOwner)
+  }
+
+  handleUserData(currentUserId: string) {
+
+    this.postId = this.route.snapshot.paramMap.get('id');
+    if(this.postId) {
+      this.apiService.getPostById(this.postId).subscribe((post: Post | undefined) => {
+        if (post) {
+          // Use the retrieved post data here
+          this.post = post;
+          if (currentUserId !== post.ownerId) {
+            this.isOwner = false;
+          } else {
+            this.isOwner = true;
+          }
+        }
+      })
+    }
+
+
     this.route.paramMap.subscribe(params => {
       const postId = params.get('id');
       if(postId) {
